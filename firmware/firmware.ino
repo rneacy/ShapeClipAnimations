74f2a2c33bf65c24359a87c3c8d96cd0f033b239
Kirirei <r.neacy@lancaster.ac.uk>
2020-01-13 18:23:03 +0000

    NeoPixel LED now indicates if ShapeClip is ready.

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,4 +1,5 @@
 #include <Stepper.h>
+#include "Adafruit_NeoPixel.h"
 
 /* SERIAL SETTINGS */
 const int BAUD_RATE = 9600;       // Baud rate to use in serial comms.
@@ -12,7 +13,8 @@ const int P_MOTOR_COIL_1_POS = 4;   // Coil 1 positive.
 const int P_MOTOR_COIL_1_NEG = 5;   // Coil 1 negative.
 const int P_MOTOR_COIL_2_POS = 6;   // Coil 2 positive.
 const int P_MOTOR_COIL_2_NEG = 7;   // Coil 2 negative.
-const int P_LED = 165;              // Onboard LED.
+const int P_LED = A5;              	// Onboard LED.
+const int P_NEOPIXEL = 9;			// Neopixel LED.
 
 /* OPERATING PARAMS */
 const int APEX_DELAY = 1000;      // Time to wait in between up and downs.
@@ -21,12 +23,14 @@ bool running = true;
 
 /* REQUIRED OBJECTS */
 Stepper stepper (MOTOR_STEPS, P_MOTOR_COIL_1_POS, P_MOTOR_COIL_1_NEG, P_MOTOR_COIL_2_POS, P_MOTOR_COIL_2_NEG);
+Adafruit_NeoPixel led (1, P_NEOPIXEL);
 
 void setup() {
-    // Set up serial comms (this would actually only be on the serial bridge firmware)
-    //pinMode(0, INPUT);
-    //pinMode(1, INPUT);
     Serial.begin(BAUD_RATE);
+
+	led.begin();
+	led.setPixelColor(0, 255, 0, 0);
+	led.show();
 
     digitalWrite(P_LED, LOW);
 	resetMotor();
@@ -34,14 +38,18 @@ void setup() {
 }
 
 void loop() {
+	led.setPixelColor(0, ((motorReady) ? led.Color(0, 255, 0) : led.Color(255, 0, 0)));
+	led.show();
+
 	if(running){
 		// Ensure motor has been set up first.
 		if(motorReady){
 			// Move up and down to max that motor can.
+			/*
 			stepper.step(MOTOR_STEPS);
 			delay(APEX_DELAY);
 			stepper.step(-MOTOR_STEPS);
-			delay(APEX_DELAY);
+			delay(APEX_DELAY);*/
 		}
 	}
 }
